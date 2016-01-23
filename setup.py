44d2f1f3dd7ffbef893858ed695f83e6c13f7065
bentejuy <bentejuy@gmail.com>
2016-01-23 14:11:21 +0000

    Updated to version 0.1.9

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_packages(path, excludes=None):
 setup(
     name = "raspybot",
     packages = find_packages('raspybot'),
-    version = "0.1.8",
+    version = "0.1.9",
     description = "Multipurpose library oriented to the Raspberry Pi.",
     author = "Bentejuy Lopez",
     author_email = "bentejuy@gmail.com",
